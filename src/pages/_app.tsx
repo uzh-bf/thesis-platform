f12f63adbe33eab0096d7aeaf7dbf893717d9b38
Roland Schlaefli <rolandschlaefli@gmail.com>
2023-03-14 11:07:54 +0100

    fix: source sans

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { ThemeProvider } from '@uzh-bf/design-system'
 import type { Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import { Source_Sans_Pro } from 'next/font/google'
+
 import { trpc } from '../lib/trpc'
+
+config.autoAddCss = false
 
 import '../globals.css'
 
-import { Source_Sans_Pro } from '@next/font/google'
-
-const sourceSans = Source_Sans_Pro({
+const sourceSansPro = Source_Sans_Pro({
   variable: '--font-source-sans',
   weight: ['400', '700'],
   subsets: ['latin'],
@@ -18,11 +23,22 @@ function App({
   pageProps: { session, ...pageProps },
 }: AppProps<{ initialGraphQLState: any; session: Session }>) {
   return (
-    <SessionProvider session={session}>
-      <div className={`${sourceSans.variable} font-sans antialiased`}>
-        <Component {...pageProps} />
-      </div>
-    </SessionProvider>
+    <div
+      id="#__app"
+      className={`${sourceSansPro.variable} font-sans antialiased h-full min-h-full`}
+    >
+      <ThemeProvider>
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </ThemeProvider>
+
+      <style jsx global>{`
+        :root {
+          --font-source-sans: ${sourceSansPro.variable};
+        }
+      `}</style>
+    </div>
   )
 }
 
