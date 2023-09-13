543cfd24e1047ac4f579b56933de8c6013108295
maximilian.weber <maximilian.weber@bf.uzh.ch>
2023-09-13 13:43:10 +0200

    chore: ProposalMeta component created

---
+++ src/components/ProposalMeta.tsx
@@ -0,0 +1,120 @@
+import { faFilePdf } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { format, parseISO } from 'date-fns'
+import Link from 'next/link'
+
+const FileTypeIconMap: Record<string, IconDefinition> = {
+  'application/pdf': faFilePdf,
+}
+function ProposalMeta({
+  proposalDetails,
+}: {
+  proposalDetails: ProposalDetails
+}) {
+  return (
+    <div className="p-4">
+      <h1 className="text-base font-bold">{proposalDetails.title}</h1>
+
+      <p className="pb-4 text-base">{proposalDetails.description}</p>
+
+      <div className="grid grid-cols-2">
+        <div className="text-base">
+          <div className="font-bold">Type of Proposal</div>
+          <div>{proposalDetails.studyLevel}</div>
+        </div>
+        <div className="text-base">
+          <div className="font-bold">Field of Research</div>
+          <div>{proposalDetails.topicArea.name}</div>
+        </div>
+        <div className="text-base">
+          <div className="font-bold">Proposal Language</div>
+          <div>{JSON.parse(proposalDetails.language).join(', ')}</div>
+        </div>
+        {proposalDetails.typeKey === 'STUDENT' && (
+          <div className="text-base">
+            <div className="font-bold">Planned Start Date</div>
+            <div>
+              {format(
+                parseISO(proposalDetails.applications[0].plannedStartAt),
+                'yyyy-MM-dd'
+              )}
+            </div>
+          </div>
+        )}
+        {proposalDetails.typeKey === 'SUPERVISOR' && (
+          <div className="text-base">
+            <div className="font-bold">Time Frame</div>
+            <div>{proposalDetails.timeFrame}</div>
+          </div>
+        )}
+        <div className="text-base">
+          <div className="font-bold">Supervised By</div>
+          <div>{proposalDetails.supervisedBy?.name ?? 'Unassigned'}</div>
+        </div>
+
+        {proposalDetails.typeKey === 'STUDENT' && (
+          <div className="text-base">
+            <div className="font-bold">Submitted By</div>
+            <div>{proposalDetails.applications[0].fullName}</div>
+          </div>
+        )}
+
+        <div className="text-base">
+          <div className="font-bold">Submitted On</div>
+          <div>{format(parseISO(proposalDetails.createdAt), 'dd.MM.Y')}</div>
+        </div>
+      </div>
+
+      {proposalDetails.typeKey === 'STUDENT' && (
+        <div className="text-sm">
+          {proposalDetails.attachments.map((attachment) => (
+            <Link
+              key={attachment.id}
+              href={attachment.href}
+              target="_blank"
+              className="hover:text-orange-600"
+            >
+              <div className="flex flex-row items-center gap-2 text-lg">
+                <FontAwesomeIcon icon={FileTypeIconMap[attachment.type]} />
+                <div>{attachment.name}</div>
+              </div>
+            </Link>
+          ))}
+          {proposalDetails.applications[0].attachments.map((attachment) => (
+            <Link
+              key={attachment.id}
+              href={attachment.href}
+              target="_blank"
+              className="hover:text-orange-600"
+            >
+              <div className="flex flex-row items-center gap-2 text-lg">
+                <FontAwesomeIcon icon={FileTypeIconMap[attachment.type]} />
+                <div>{attachment.name}</div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
+
+      {proposalDetails.typeKey === 'SUPERVISOR' && (
+        <div className="flex flex-row gap-6 text-sm">
+          {proposalDetails.attachments.map((attachment) => (
+            <Link
+              key={attachment.id}
+              href={attachment.href}
+              target="_blank"
+              className="hover:text-orange-600"
+            >
+              <div className="flex flex-row items-center gap-2 text-lg">
+                <FontAwesomeIcon icon={FileTypeIconMap[attachment.type]} />
+                <div>{attachment.name}</div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ProposalMeta
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,13 +22,13 @@ import { add, format, parseISO } from 'date-fns'
 import dayjs from 'dayjs'
 import { Form, Formik } from 'formik'
 import { useSession } from 'next-auth/react'
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 import * as R from 'ramda'
 import { useMemo, useRef, useState } from 'react'
 import Dropzone from 'react-dropzone'
 import toast, { Toaster } from 'react-hot-toast'
 import Header from 'src/components/Header'
+import ProposalMeta from 'src/components/Proposalmeta'
 import { ProposalType, UserRole } from 'src/lib/constants'
 import { trpc } from 'src/lib/trpc'
 import { AppRouter } from 'src/server/routers/_app'
@@ -41,117 +41,6 @@ type ProposalDetails = Proposals[number]
 
 const FileTypeIconMap: Record<string, IconDefinition> = {
   'application/pdf': faFilePdf,
-}
-
-function ProposalMeta({
-  proposalDetails,
-}: {
-  proposalDetails: ProposalDetails
-}) {
-  return (
-    <div className="p-4">
-      <h1 className="text-base font-bold">{proposalDetails.title}</h1>
-
-      <p className="pb-4 text-base">{proposalDetails.description}</p>
-
-      <div className="grid grid-cols-2">
-        <div className="text-base">
-          <div className="font-bold">Type of Proposal</div>
-          <div>{proposalDetails.studyLevel}</div>
-        </div>
-        <div className="text-base">
-          <div className="font-bold">Field of Research</div>
-          <div>{proposalDetails.topicArea.name}</div>
-        </div>
-        <div className="text-base">
-          <div className="font-bold">Proposal Language</div>
-          <div>{JSON.parse(proposalDetails.language).join(', ')}</div>
-        </div>
-        {proposalDetails.typeKey === 'STUDENT' && (
-          <div className="text-base">
-            <div className="font-bold">Planned Start Date</div>
-            <div>
-              {format(
-                parseISO(proposalDetails.applications[0].plannedStartAt),
-                'yyyy-MM-dd'
-              )}
-            </div>
-          </div>
-        )}
-        {proposalDetails.typeKey === 'SUPERVISOR' && (
-          <div className="text-base">
-            <div className="font-bold">Time Frame</div>
-            <div>{proposalDetails.timeFrame}</div>
-          </div>
-        )}
-        <div className="text-base">
-          <div className="font-bold">Supervised By</div>
-          <div>{proposalDetails.supervisedBy?.name ?? 'Unassigned'}</div>
-        </div>
-
-        {proposalDetails.typeKey === 'STUDENT' && (
-          <div className="text-base">
-            <div className="font-bold">Submitted By</div>
-            <div>{proposalDetails.applications[0].fullName}</div>
-          </div>
-        )}
-
-        <div className="text-base">
-          <div className="font-bold">Submitted On</div>
-          <div>{format(parseISO(proposalDetails.createdAt), 'dd.MM.Y')}</div>
-        </div>
-      </div>
-
-      {proposalDetails.typeKey === 'STUDENT' && (
-        <div className="text-sm">
-          {proposalDetails.attachments.map((attachment) => (
-            <Link
-              key={attachment.id}
-              href={attachment.href}
-              target="_blank"
-              className="hover:text-orange-600"
-            >
-              <div className="flex flex-row items-center gap-2 text-lg">
-                <FontAwesomeIcon icon={FileTypeIconMap[attachment.type]} />
-                <div>{attachment.name}</div>
-              </div>
-            </Link>
-          ))}
-          {proposalDetails.applications[0].attachments.map((attachment) => (
-            <Link
-              key={attachment.id}
-              href={attachment.href}
-              target="_blank"
-              className="hover:text-orange-600"
-            >
-              <div className="flex flex-row items-center gap-2 text-lg">
-                <FontAwesomeIcon icon={FileTypeIconMap[attachment.type]} />
-                <div>{attachment.name}</div>
-              </div>
-            </Link>
-          ))}
-        </div>
-      )}
-
-      {proposalDetails.typeKey === 'SUPERVISOR' && (
-        <div className="flex flex-row gap-6 text-sm">
-          {proposalDetails.attachments.map((attachment) => (
-            <Link
-              key={attachment.id}
-              href={attachment.href}
-              target="_blank"
-              className="hover:text-orange-600"
-            >
-              <div className="flex flex-row items-center gap-2 text-lg">
-                <FontAwesomeIcon icon={FileTypeIconMap[attachment.type]} />
-                <div>{attachment.name}</div>
-              </div>
-            </Link>
-          ))}
-        </div>
-      )}
-    </div>
-  )
 }
 
 function ApplicationForm({ proposalName, proposalId }) {
