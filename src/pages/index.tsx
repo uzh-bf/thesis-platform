99e318c88368c0f250e64383168884642a2670f7
maximilian.weber <maximilian.weber@bf.uzh.ch>
2023-09-13 14:04:51 +0200

    chore: DeclineProposalForm component added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,25 +6,15 @@ import {
 } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { inferProcedureOutput } from '@trpc/server'
-import {
-  Button,
-  FormikSelectField,
-  FormikTextField,
-  FormikTextareaField,
-  H2,
-  H3,
-  Table,
-  Tabs,
-} from '@uzh-bf/design-system'
+import { Button, H2, H3, Table, Tabs } from '@uzh-bf/design-system'
 import { add, format, parseISO } from 'date-fns'
-import { Form, Formik } from 'formik'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import * as R from 'ramda'
 import { useMemo, useRef, useState } from 'react'
-import toast, { Toaster } from 'react-hot-toast'
 import AcceptProposalForm from 'src/components/AcceptProposalForm'
 import ApplicationForm from 'src/components/ApplicationForm'
+import DeclineProposalForm from 'src/components/DeclineProposalForm'
 import Header from 'src/components/Header'
 import ProposalMeta from 'src/components/ProposalMeta'
 import RejectProposalForm from 'src/components/RejectProposalForm'
@@ -33,108 +23,12 @@ import { ProposalType, UserRole } from 'src/lib/constants'
 import { trpc } from 'src/lib/trpc'
 import { AppRouter } from 'src/server/routers/_app'
 import { twMerge } from 'tailwind-merge'
-import * as Yup from 'yup'
 
 type Proposals = inferProcedureOutput<AppRouter['proposals']>
 type ProposalDetails = Proposals[number]
 
 const FileTypeIconMap: Record<string, IconDefinition> = {
   'application/pdf': faFilePdf,
-}
-
-function DeclineProposalForm({ proposalName, proposalId, supervisorEmail }) {
-  const SignupSchema = Yup.object().shape({
-    reason: Yup.string().required('Required'),
-    comment: Yup.string().required('Required'),
-  })
-
-  return (
-    <Formik
-      initialValues={{
-        proposalName: proposalName,
-        reason: '',
-        comment: '',
-        proposalId: proposalId,
-        supervisorEmail: supervisorEmail,
-        actionType: 'DECLINE',
-      }}
-      validationSchema={SignupSchema}
-      onSubmit={async (values, { resetForm }) => {
-        console.log(JSON.stringify(values)),
-          await fetch(process.env.NEXT_PUBLIC_PROPOSAL_FEEDBACK_URL as string, {
-            method: 'POST',
-            headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(values),
-          })
-        resetForm()
-        toast.success('Proposal declined successfully!')
-      }}
-    >
-      <Form>
-        <div>
-          Rejecting this proposal because of lacking content quality or format
-          requirements will cause review by the thesis coordinator. The student
-          will need to improve and resubmit the proposal.
-        </div>
-        <div className="grid mt-4 place-items-lef">
-          <FormikTextField
-            disabled={true}
-            name="proposalName"
-            label="Proposal"
-            className={{
-              label: 'font-sans text-lg',
-              field: 'flex-col',
-            }}
-          />
-          <FormikSelectField
-            required
-            name="reason"
-            items={[
-              {
-                value: 'PERSONAL_WORKLOAD',
-                label: 'Personal workload too high',
-              },
-              {
-                value: 'LACKING_INTEREST',
-                label: 'Lack of interest in specific topic',
-              },
-              {
-                value: 'LANGUAGE',
-                label: 'Mismatch in language',
-              },
-            ]}
-            label="Reason"
-            placeholder="Select a name"
-            className={{
-              label: 'font-sans text-lg',
-              root: 'flex-col',
-            }}
-          />
-          <FormikTextareaField
-            required
-            name="comment"
-            label="Comment"
-            className={{
-              label: 'font-sans text-lg',
-              field: 'flex-col',
-            }}
-          />
-          <div className="mt-2 italic">
-            Why do you decline this proposal specifcally? Your comment will not
-            be shown to the student.
-          </div>
-        </div>
-
-        <Button className={{ root: 'mt-2' }} type="submit">
-          Decline Proposal
-        </Button>
-        <Toaster />
-      </Form>
-    </Formik>
-  )
 }
 
 function CreateStudentProposal({ ref }) {
