34251623a4b57ff008fe35278020f528e84011d4
maximilian.weber <maximilian.weber@bf.uzh.ch>
2023-09-13 13:59:16 +0200

    chore: tentativeAcceptProposalForm added

---
+++ src/components/TentativeAcceptProposalForm.tsx
@@ -0,0 +1,83 @@
+import {
+  Button,
+  FormikTextField,
+  FormikTextareaField,
+} from '@uzh-bf/design-system'
+import { Form, Formik } from 'formik'
+import toast, { Toaster } from 'react-hot-toast'
+import * as Yup from 'yup'
+
+function TentativeAcceptProposalForm({
+  proposalName,
+  proposalId,
+  supervisorEmail,
+}) {
+  const SignupSchema = Yup.object().shape({
+    comment: Yup.string().required('Required'),
+  })
+
+  return (
+    <Formik
+      initialValues={{
+        proposalName: proposalName,
+        comment: '',
+        proposalId: proposalId,
+        supervisorEmail: supervisorEmail,
+        actionType: 'ACCEPT_TENTATIVE',
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={async (values, { resetForm }) => {
+        console.log(JSON.stringify(values)),
+          await fetch(process.env.NEXT_PUBLIC_PROPOSAL_FEEDBACK_URL as string, {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(values),
+          })
+        resetForm()
+        toast.success('Proposal tentatively accepted successfully!')
+      }}
+    >
+      <Form>
+        <div>
+          When you accept the proposal tentatively, the student will receive
+          your feedback and is required to improve the proposal before you
+          finally accept the proposal for supervision.
+        </div>
+        <div className="grid mt-4 place-items-lef">
+          <FormikTextField
+            disabled={true}
+            name="proposalName"
+            label="Proposal"
+            className={{
+              label: 'font-sans text-lg',
+              field: 'flex-col',
+            }}
+          />
+          <FormikTextareaField
+            required
+            name="comment"
+            label="Feedback"
+            className={{
+              label: 'font-sans text-lg',
+              field: 'flex-col',
+            }}
+          />
+          <div className="mt-2 italic">
+            Your message will be sent to the student alongside your notification
+            of interest.
+          </div>
+        </div>
+
+        <Button className={{ root: 'mt-2' }} type="submit">
+          Accept Proposal (Tentative)
+        </Button>
+        <Toaster />
+      </Form>
+    </Formik>
+  )
+}
+
+export default TentativeAcceptProposalForm
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,6 +27,7 @@ import AcceptProposalForm from 'src/components/AcceptProposalForm'
 import ApplicationForm from 'src/components/ApplicationForm'
 import Header from 'src/components/Header'
 import ProposalMeta from 'src/components/ProposalMeta'
+import TentativeAcceptProposalForm from 'src/components/TentativeAcceptProposalForm'
 import { ProposalType, UserRole } from 'src/lib/constants'
 import { trpc } from 'src/lib/trpc'
 import { AppRouter } from 'src/server/routers/_app'
@@ -38,79 +39,6 @@ type ProposalDetails = Proposals[number]
 
 const FileTypeIconMap: Record<string, IconDefinition> = {
   'application/pdf': faFilePdf,
-}
-
-function TentativeAcceptProposalForm({
-  proposalName,
-  proposalId,
-  supervisorEmail,
-}) {
-  const SignupSchema = Yup.object().shape({
-    comment: Yup.string().required('Required'),
-  })
-
-  return (
-    <Formik
-      initialValues={{
-        proposalName: proposalName,
-        comment: '',
-        proposalId: proposalId,
-        supervisorEmail: supervisorEmail,
-        actionType: 'ACCEPT_TENTATIVE',
-      }}
-      validationSchema={SignupSchema}
-      onSubmit={async (values, { resetForm }) => {
-        console.log(JSON.stringify(values)),
-          await fetch(process.env.NEXT_PUBLIC_PROPOSAL_FEEDBACK_URL as string, {
-            method: 'POST',
-            headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(values),
-          })
-        resetForm()
-        toast.success('Proposal tentatively accepted successfully!')
-      }}
-    >
-      <Form>
-        <div>
-          When you accept the proposal tentatively, the student will receive
-          your feedback and is required to improve the proposal before you
-          finally accept the proposal for supervision.
-        </div>
-        <div className="grid mt-4 place-items-lef">
-          <FormikTextField
-            disabled={true}
-            name="proposalName"
-            label="Proposal"
-            className={{
-              label: 'font-sans text-lg',
-              field: 'flex-col',
-            }}
-          />
-          <FormikTextareaField
-            required
-            name="comment"
-            label="Feedback"
-            className={{
-              label: 'font-sans text-lg',
-              field: 'flex-col',
-            }}
-          />
-          <div className="mt-2 italic">
-            Your message will be sent to the student alongside your notification
-            of interest.
-          </div>
-        </div>
-
-        <Button className={{ root: 'mt-2' }} type="submit">
-          Accept Proposal (Tentative)
-        </Button>
-        <Toaster />
-      </Form>
-    </Formik>
-  )
 }
 
 function RejectProposalForm({ proposalName, proposalId, supervisorEmail }) {
