70290abcd5caeaa4aff3e4b675a1a7545c21fd7d
maximilian.weber <maximilian.weber@bf.uzh.ch>
2023-09-13 13:15:08 +0200

    chore: own component for header

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import { Button, H1 } from '@uzh-bf/design-system'
+import { signIn, signOut, useSession } from 'next-auth/react'
+
+function Header() {
+  const { data: session } = useSession()
+
+  return (
+    <header className="">
+      <div className="flex flex-col p-4 text-gray-600 bg-gray-200 md:justify-between md:flex-row">
+        <H1 className={{ root: 'text-3xl' }}>DBF Thesis Market</H1>
+        <div className="md:flex md:items-center">
+          {session?.user ? (
+            <>
+              <div className="text-sm md:pr-2">
+                Signed in as {session.user.email} ({session.user.role})
+              </div>
+              <Button onClick={() => signOut()}>Sign out</Button>
+            </>
+          ) : (
+            <>
+              <Button onClick={() => signIn()}>DBF Supervisor Log-in</Button>
+            </>
+          )}
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,6 @@ import {
   FormikSelectField,
   FormikTextField,
   FormikTextareaField,
-  H1,
   H2,
   H3,
   Table,
@@ -22,13 +21,14 @@ import {
 import { add, format, parseISO } from 'date-fns'
 import dayjs from 'dayjs'
 import { Form, Formik } from 'formik'
-import { signIn, signOut, useSession } from 'next-auth/react'
+import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import * as R from 'ramda'
 import { useMemo, useRef, useState } from 'react'
 import Dropzone from 'react-dropzone'
 import toast, { Toaster } from 'react-hot-toast'
+import Header from 'src/components/Header'
 import { ProposalType, UserRole } from 'src/lib/constants'
 import { trpc } from 'src/lib/trpc'
 import { AppRouter } from 'src/server/routers/_app'
@@ -41,32 +41,6 @@ type ProposalDetails = Proposals[number]
 
 const FileTypeIconMap: Record<string, IconDefinition> = {
   'application/pdf': faFilePdf,
-}
-
-function Header() {
-  const { data: session } = useSession()
-
-  return (
-    <header className="">
-      <div className="flex flex-col p-4 text-gray-600 bg-gray-200 md:justify-between md:flex-row">
-        <H1 className={{ root: 'text-3xl' }}>DBF Thesis Market</H1>
-        <div className="md:flex md:items-center">
-          {session?.user ? (
-            <>
-              <div className="text-sm md:pr-2">
-                Signed in as {session.user.email} ({session.user.role})
-              </div>
-              <Button onClick={() => signOut()}>Sign out</Button>
-            </>
-          ) : (
-            <>
-              <Button onClick={() => signIn()}>DBF Supervisor Log-in</Button>
-            </>
-          )}
-        </div>
-      </div>
-    </header>
-  )
 }
 
 function ProposalMeta({
