250c9bf172755933da0cae7846e0da2eed5f9d0c
mxmlnwbr <maximilian.weber@bluewin.ch>
2023-10-18 15:15:55 +0200

    proposalstatus added

---
+++ src/components/ProposalStatusForm.tsx
@@ -0,0 +1,146 @@
+import { Tabs } from '@uzh-bf/design-system'
+import AcceptProposalForm from './AcceptProposalForm' // Import AcceptProposalForm and other form components
+import DeclineProposalForm from './DeclineProposalForm'
+import RejectProposalForm from './RejectProposalForm'
+import TentativeAcceptProposalForm from './TentativeAcceptProposalForm'
+
+export default function ProposalStatusForm({ proposalDetails, session }) {
+  if (
+    proposalDetails?.typeKey === 'STUDENT' &&
+    proposalDetails?.statusKey === 'MATCHED_TENTATIVE'
+  ) {
+    return (
+      <>
+        <div className="pl-4 bg-red-100">
+          <b>
+            This proposal is tentatively matched with a student. Please accept
+            or reject the proposal.
+          </b>
+        </div>
+        <div className="">
+          <Tabs defaultValue="accept">
+            <Tabs.TabList className={{ root: 'md:flex-row border' }}>
+              <Tabs.Tab key="accept" value="accept" label="Accept" />
+              <Tabs.Tab key="reject" value="reject" label="Reject" />
+            </Tabs.TabList>
+            <Tabs.TabContent
+              key="accept"
+              value="accept"
+              className={{
+                root: 'border border-t-0 rounded-none px-4',
+              }}
+            >
+              <AcceptProposalForm
+                key={proposalDetails?.id}
+                proposalName={proposalDetails?.title}
+                proposalId={proposalDetails?.id}
+                supervisorEmail={session?.user?.email}
+              />
+            </Tabs.TabContent>
+            <Tabs.TabContent
+              key="reject"
+              value="reject"
+              className={{
+                root: 'border border-t-0 rounded-none px-4',
+              }}
+            >
+              <RejectProposalForm
+                key={proposalDetails?.id}
+                proposalName={proposalDetails?.title}
+                proposalId={proposalDetails?.id}
+                supervisorEmail={session?.user?.email}
+              />
+            </Tabs.TabContent>
+          </Tabs>
+        </div>
+      </>
+    )
+  } else if (
+    (proposalDetails?.typeKey === 'STUDENT' &&
+      proposalDetails?.statusKey === 'MATCHED') ||
+    proposalDetails?.receivedFeedbacks?.length > 0
+  ) {
+    return (
+      <div className="p-4 bg-yellow-100">
+        {proposalDetails?.applications?.[0].statusKey === 'ACCEPTED'
+          ? 'You have already accepted this proposal!'
+          : 'You have already provided feedback to this proposal!'}
+      </div>
+    )
+  } else if (proposalDetails?.typeKey === 'STUDENT') {
+    return (
+      <div className="">
+        <Tabs defaultValue="accept">
+          <Tabs.TabList className={{ root: 'md:flex-row border' }}>
+            <Tabs.Tab key="accept" value="accept" label="Accept" />
+            <Tabs.Tab
+              key="acceptTentative"
+              value="acceptTentative"
+              label="Accept (Tentative)"
+            />
+            <Tabs.Tab key="reject" value="reject" label="Reject" />
+            <Tabs.Tab key="decline" value="decline" label="Decline" />
+          </Tabs.TabList>
+          <Tabs.TabContent
+            key="accept"
+            value="accept"
+            className={{
+              root: 'border border-t-0 rounded-none px-4',
+            }}
+          >
+            <AcceptProposalForm
+              key={proposalDetails?.id}
+              proposalName={proposalDetails?.title}
+              proposalId={proposalDetails?.id}
+              supervisorEmail={session?.user?.email}
+            />
+          </Tabs.TabContent>
+          <Tabs.TabContent
+            key="acceptTentative"
+            value="acceptTentative"
+            className={{
+              root: 'border border-t-0 rounded-none px-4',
+            }}
+          >
+            <TentativeAcceptProposalForm
+              key={proposalDetails?.id}
+              proposalName={proposalDetails?.title}
+              proposalId={proposalDetails?.id}
+              supervisorEmail={session?.user?.email}
+            />
+          </Tabs.TabContent>
+          <Tabs.TabContent
+            key="decline"
+            value="decline"
+            className={{
+              root: 'border border-t-0 rounded-none px-4',
+            }}
+          >
+            <DeclineProposalForm
+              key={proposalDetails?.id}
+              proposalName={proposalDetails?.title}
+              proposalId={proposalDetails?.id}
+              supervisorEmail={session?.user?.email}
+            />
+          </Tabs.TabContent>
+          <Tabs.TabContent
+            key="reject"
+            value="reject"
+            className={{
+              root: 'border border-t-0 rounded-none px-4',
+            }}
+          >
+            <RejectProposalForm
+              key={proposalDetails?.id}
+              proposalName={proposalDetails?.title}
+              proposalId={proposalDetails?.id}
+              supervisorEmail={session?.user?.email}
+            />
+          </Tabs.TabContent>
+        </Tabs>
+      </div>
+    )
+  } else {
+    return null
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,24 @@
 import { IconDefinition, faFilePdf } from '@fortawesome/free-regular-svg-icons'
-import { Tabs } from '@uzh-bf/design-system'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import * as R from 'ramda'
 import { useMemo, useRef, useState } from 'react'
-import AcceptProposalForm from 'src/components/AcceptProposalForm'
 import CreateProposal from 'src/components/CreateProposal'
-import DeclineProposalForm from 'src/components/DeclineProposalForm'
 import Header from 'src/components/Header'
 import NewProposalButton from 'src/components/NewProposalButton'
 import ProposalApplication from 'src/components/ProposalApplication'
 import ProposalFeedback from 'src/components/ProposalFeedback'
 import ProposalMeta from 'src/components/ProposalMeta'
-import RejectProposalForm from 'src/components/RejectProposalForm'
+import ProposalStatusForm from 'src/components/ProposalStatusForm'
 import StudentProposals from 'src/components/StudentProposals'
 import SupervisorProposals from 'src/components/SupervisorProposals'
-import TentativeAcceptProposalForm from 'src/components/TentativeAcceptProposalForm'
 import { UserRole } from 'src/lib/constants'
 import { trpc } from 'src/lib/trpc'
 
 const FileTypeIconMap: Record<string, IconDefinition> = {
   'application/pdf': faFilePdf,
 }
-function Index() {
+export default function Index() {
   const router = useRouter()
   const buttonRef = useRef<null | HTMLDivElement>(null)
 
@@ -110,139 +106,12 @@ function Index() {
             isSupervisor={isSupervisor}
             isAdmin={isAdmin}
           />
-
-          {proposalDetails?.typeKey === 'STUDENT' &&
-          proposalDetails?.statusKey === 'MATCHED_TENTATIVE' ? (
-            <>
-              <div className="pl-4 bg-red-100">
-                <b>
-                  This proposal is tentatively matched with a student. Please
-                  accept or reject the proposal.
-                </b>
-              </div>
-              <div className="">
-                <Tabs defaultValue="accept">
-                  <Tabs.TabList className={{ root: 'md:flex-row border' }}>
-                    <Tabs.Tab key="accept" value="accept" label="Accept" />
-                    <Tabs.Tab key="reject" value="reject" label="Reject" />
-                  </Tabs.TabList>
-                  <Tabs.TabContent
-                    key="accept"
-                    value="accept"
-                    className={{
-                      root: 'border border-t-0 rounded-none px-4',
-                    }}
-                  >
-                    <AcceptProposalForm
-                      key={proposalDetails?.id}
-                      proposalName={proposalDetails?.title}
-                      proposalId={proposalDetails?.id}
-                      supervisorEmail={session?.user?.email}
-                    />
-                  </Tabs.TabContent>
-                  <Tabs.TabContent
-                    key="reject"
-                    value="reject"
-                    className={{
-                      root: 'border border-t-0 rounded-none px-4',
-                    }}
-                  >
-                    <RejectProposalForm
-                      key={proposalDetails?.id}
-                      proposalName={proposalDetails?.title}
-                      proposalId={proposalDetails?.id}
-                      supervisorEmail={session?.user?.email}
-                    />
-                  </Tabs.TabContent>
-                </Tabs>
-              </div>
-            </>
-          ) : (proposalDetails?.typeKey === 'STUDENT' &&
-              proposalDetails?.statusKey === 'MATCHED') ||
-            proposalDetails?.receivedFeedbacks?.length > 0 ? (
-            <div className="p-4 bg-yellow-100">
-              {proposalDetails?.applications?.[0].statusKey === 'ACCEPTED'
-                ? 'You have already accepted this proposal!'
-                : 'You have already provided feedback to this proposal!'}
-            </div>
-          ) : (
-            proposalDetails?.typeKey === 'STUDENT' && (
-              <div className="">
-                <Tabs defaultValue="accept">
-                  <Tabs.TabList className={{ root: 'md:flex-row border' }}>
-                    <Tabs.Tab key="accept" value="accept" label="Accept" />
-                    <Tabs.Tab
-                      key="acceptTentative"
-                      value="acceptTentative"
-                      label="Accept (Tentative)"
-                    />
-                    <Tabs.Tab key="reject" value="reject" label="Reject" />
-                    <Tabs.Tab key="decline" value="decline" label="Decline" />
-                  </Tabs.TabList>
-                  <Tabs.TabContent
-                    key="accept"
-                    value="accept"
-                    className={{
-                      root: 'border border-t-0 rounded-none px-4',
-                    }}
-                  >
-                    <AcceptProposalForm
-                      key={proposalDetails?.id}
-                      proposalName={proposalDetails?.title}
-                      proposalId={proposalDetails?.id}
-                      supervisorEmail={session?.user?.email}
-                    />
-                  </Tabs.TabContent>
-                  <Tabs.TabContent
-                    key="acceptTentative"
-                    value="acceptTentative"
-                    className={{
-                      root: 'border border-t-0 rounded-none px-4',
-                    }}
-                  >
-                    <TentativeAcceptProposalForm
-                      key={proposalDetails?.id}
-                      proposalName={proposalDetails?.title}
-                      proposalId={proposalDetails?.id}
-                      supervisorEmail={session?.user?.email}
-                    />
-                  </Tabs.TabContent>
-                  <Tabs.TabContent
-                    key="decline"
-                    value="decline"
-                    className={{
-                      root: 'border border-t-0 rounded-none px-4',
-                    }}
-                  >
-                    <DeclineProposalForm
-                      key={proposalDetails?.id}
-                      proposalName={proposalDetails?.title}
-                      proposalId={proposalDetails?.id}
-                      supervisorEmail={session?.user?.email}
-                    />
-                  </Tabs.TabContent>
-                  <Tabs.TabContent
-                    key="reject"
-                    value="reject"
-                    className={{
-                      root: 'border border-t-0 rounded-none px-4',
-                    }}
-                  >
-                    <RejectProposalForm
-                      key={proposalDetails?.id}
-                      proposalName={proposalDetails?.title}
-                      proposalId={proposalDetails?.id}
-                      supervisorEmail={session?.user?.email}
-                    />
-                  </Tabs.TabContent>
-                </Tabs>
-              </div>
-            )
-          )}
+          <ProposalStatusForm
+            proposalDetails={proposalDetails}
+            session={session}
+          />
         </div>
       </div>
     </div>
   )
 }
-
-export default Index
