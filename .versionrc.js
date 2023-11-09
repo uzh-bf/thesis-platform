3cd801649da95c8a91a896bf60486acc3df1cad0
Roland Schläfli <rolandschlaefli@gmail.com>
2023-11-09 10:25:25 +0100

    chore: add standard-version and release commands (#8)

---
+++ .versionrc.js
@@ -0,0 +1,76 @@
+module.exports = {
+  packageFiles: [
+    {
+      filename: `package.json`,
+      type: 'json',
+    },
+  ],
+  bumpFiles: [''].reduce(
+    (acc, path) => {
+      return acc.concat({
+        filename: `${path}package.json`,
+        type: 'json',
+      })
+    },
+    [
+      {
+        filename: `package-lock.json`,
+        type: 'json',
+      },
+    ]
+  ),
+  types: [
+    {
+      type: 'feat',
+      section: 'Features',
+    },
+    {
+      type: 'enhance',
+      section: 'Enhancements',
+    },
+    {
+      type: 'fix',
+      section: 'Bug Fixes',
+    },
+    {
+      type: 'docs',
+      section: 'Documentation',
+    },
+    {
+      type: 'refactor',
+      section: 'Refactors',
+    },
+    {
+      type: 'perf',
+      section: 'Performance',
+    },
+    {
+      type: 'deploy',
+      section: 'Deployment',
+    },
+    {
+      type: 'deps',
+      section: 'Dependencies',
+    },
+    {
+      type: 'build',
+      section: 'Build and CI',
+    },
+    {
+      type: 'ci',
+      section: 'Build and CI',
+    },
+    {
+      type: 'wip',
+      section: 'Other',
+    },
+    {
+      type: 'test',
+      section: 'Other',
+    },
+    {
+      type: 'style',
+      section: 'Other',
+    },
+  ],
+}
