221716e0997d251522128fe9e67a60aa09bb6198
mxmlnwbr <71628027+mxmlnwbr@users.noreply.github.com>
2024-12-17 13:37:01 +0100

    enhance: add ARCHIVED status to prisma schema, clean up seed (#37)

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -17,247 +17,53 @@ const prismaClient = new PrismaClient({
 async function seed(prisma: PrismaClient) {
   console.log('> running prisma seed')
 
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  const applicationStatus = await prisma.applicationStatus.createMany({
+    data: Object.values(ApplicationStatus).map((status) => ({
+      key: status,
+    })),
   })
 
-  await Promise.all(
-    Object.entries(TopicAreas).map(([slug, name]) =>
-      prisma.topicArea.upsert({
-        where: { slug },
-        create: { slug, name },
-        update: {},
-      })
-    )
-  )
-
-  await Promise.all(
-    Object.values(ProposalStatus).map((status) =>
-      prisma.proposalStatus.upsert({
-        where: { key: status },
-        create: { key: status },
-        update: {},
-      })
-    )
-  )
-
-  await Promise.all(
-    Object.values(ApplicationStatus).map((status) =>
-      prisma.applicationStatus.upsert({
-        where: { key: status },
-        create: { key: status },
-        update: {},
-      })
-    )
-  )
-
-  await Promise.all(
-    Object.values(ProposalType).map((status) =>
-      prisma.proposalType.upsert({
-        where: { key: status },
-        create: { key: status },
-        update: {},
-      })
-    )
-  )
-
-  await Promise.all(
-    Object.values(ProposalFeedbackType).map((status) =>
-      prisma.proposalFeedbackType.upsert({
-        where: { key: status },
-        create: { key: status },
-        update: {},
-      })
-    )
-  )
-
-  await new Promise((resolve) =>
-    rl.question('Please sign-in before continuing...', (ans) => {
-      rl.close()
-      resolve(ans)
-    })
-  )
-
-  const user = await prisma.user.update({
-    where: { email: process.env.USER_EMAIL },
-    data: {
-      role: UserRole.SUPERVISOR,
-      name: process.env.USER_NAME as string,
-    },
+  const proposalStatus = await prisma.proposalStatus.createMany({
+    data: Object.values(ProposalStatus).map((status) => ({
+      key: status,
+    })),
   })
 
-  // await prisma.proposal.upsert({
-  //   where: { id: '3ef84a3b-cff0-4350-b760-4c5bb3b3c98f' },
-  //   create: {
-  //     id: '3ef84a3b-cff0-4350-b760-4c5bb3b3c98f',
-  //     title: 'Effects of Interest Rate on Happiness',
-  //     description: 'This is a very interesting topic',
-  //     language: '["English"]',
-  //     studyLevel: 'Master Thesis (30 ECTS)',
-  //     topicArea: {
-  //       connect: {
-  //         slug: 'sustainable_finance',
-  //       },
-  //     },
-  //     status: {
-  //       connect: { key: ProposalStatus.OPEN },
-  //     },
-  //     type: {
-  //       connect: { key: ProposalType.STUDENT },
-  //     },
-  //     applications: {
-  //       create: {
-  //         email: 'roland.ferdinand@uzh.ch',
-  //         plannedStartAt: new Date(),
-  //         fullName: 'Roland Ferdinand',
-  //         matriculationNumber: '12-345-678',
-  //         motivation: 'I want to',
-  //       },
-  //     },
-  //     ownedByStudent: 'roland.ferdinand@uzh.ch',
-  //     ownedByUser: {
-  //       connect: { email: user.email },
-  //     },
-  //     receivedFeedbacks: {
-  //       create: {
-  //         comment: 'Rejected because',
-  //         type: {
-  //           connect: { key: ProposalFeedbackType.REJECTED_NOT_SCIENTIFIC },
-  //         },
-  //         reason: ProposalFeedbackType.REJECTED_NOT_SCIENTIFIC,
-  //         user: {
-  //           connect: { email: user.email },
-  //         },
-  //       },
-  //     },
-  //   },
-  //   update: {},
-  // })
+  const proposalType = await prisma.proposalType.createMany({
+    data: Object.values(ProposalType).map((status) => ({
+      key: status,
+    })),
+  })  
 
-  // await prisma.proposal.upsert({
-  //   where: { id: '33a9a1b7-cad7-46e7-8b72-cfcbdbaa60d6' },
-  //   create: {
-  //     id: '33a9a1b7-cad7-46e7-8b72-cfcbdbaa60d6',
-  //     title:
-  //       'The role of interest rate expectations for the choice between Fixed-Rate Mortgages and Adjustable-Rate Mortgages',
-  //     description:
-  //       'The role of interest rate expectations for the choice between Fixed-Rate Mortgages and Adjustable-Rate Mortgages',
-  //     language: '["English", "German"]',
-  //     studyLevel: 'Master Thesis (30 ECTS)',
-  //     timeFrame: 'Sometime next year.',
-  //     topicArea: {
-  //       connect: {
-  //         slug: 'banking_and_insurance',
-  //       },
-  //     },
-  //     status: {
-  //       connect: { key: ProposalStatus.OPEN },
-  //     },
-  //     type: {
-  //       connect: { key: ProposalType.SUPERVISOR },
-  //     },
-  //     supervisedBy: {
-  //       create: {
-  //         supervisor: {
-  //           connect: { email: user.email },
-  //         },
-  //       },
-  //     },
-  //     ownedByUser: {
-  //       connect: { email: user.email },
-  //     },
-  //     applications: {
-  //       create: {
-  //         plannedStartAt: new Date(),
-  //         email: 'roland.ferdinand@uzh.ch',
-  //         fullName: 'Roland Ferdinand',
-  //         matriculationNumber: '12-345-678',
-  //         motivation:
-  //           'I want to do this topic as I think it is very interesting.',
-  //         attachments: {
-  //           createMany: {
-  //             data: [
-  //               {
-  //                 name: 'CV.pdf',
-  //                 href: 'https://example.com/cv.pdf',
-  //                 type: 'application/pdf',
-  //               },
-  //               {
-  //                 name: 'Transcript.pdf',
-  //                 href: 'https://example.com/cv.pdf',
-  //                 type: 'application/pdf',
-  //               },
-  //             ],
-  //           },
-  //         },
-  //       },
-  //     },
-  //   },
-  //   update: {},
-  // })
+  const topicArea = await prisma.topicArea.createMany({
+    data: Object.entries(TopicAreas).map(([slug, name]) => ({
+      slug,
+      name,
+    })),
+  })
 
-  // await prisma.proposal.upsert({
-  //   where: { id: '21140e2e-e630-494a-ab18-374fd11c62c0' },
-  //   create: {
-  //     id: '21140e2e-e630-494a-ab18-374fd11c62c0',
-  //     title:
-  //       'Treiber der Inflation in der Schweiz / Inflationsmessung in der Schweiz',
-  //     description:
-  //       'Treiber der Inflation in der Schweiz / Inflationsmessung in der Schweiz',
-  //     language: '["English"]',
-  //     studyLevel: 'Master Thesis (30 ECTS)',
-  //     timeFrame: '2023',
-  //     topicArea: {
-  //       connect: {
-  //         slug: 'financial_economics',
-  //       },
-  //     },
-  //     status: {
-  //       connect: { key: ProposalStatus.OPEN },
-  //     },
-  //     type: {
-  //       connect: { key: ProposalType.SUPERVISOR },
-  //     },
-  //     supervisedBy: {
-  //       create: {
-  //         supervisor: {
-  //           connect: { email: user.email },
-  //         },
-  //       },
-  //     },
-  //     ownedByUser: {
-  //       connect: { email: user.email },
-  //     },
-  //     applications: {
-  //       create: {
-  //         plannedStartAt: new Date(),
-  //         email: 'roland.ferdinand@uzh.ch',
-  //         fullName: 'Roland Ferdinand',
-  //         matriculationNumber: '12-345-678',
-  //         motivation:
-  //           'I want to do this topic as I think it is very interesting.',
-  //         attachments: {
-  //           createMany: {
-  //             data: [
-  //               {
-  //                 name: 'CV.pdf',
-  //                 href: 'https://example.com/cv.pdf',
-  //                 type: 'application/pdf',
-  //               },
-  //               {
-  //                 name: 'Transcript.pdf',
-  //                 href: 'https://example.com/cv.pdf',
-  //                 type: 'application/pdf',
-  //               },
-  //             ],
-  //           },
-  //         },
-  //       },
-  //     },
-  //   },
-  //   update: {},
-  // })
+  const proposalFeedbackType = await prisma.proposalFeedbackType.createMany({
+    data: Object.values(ProposalFeedbackType).map((status) => ({
+      key: status,
+    })),
+  })
+
+  const responsible = await prisma.responsible.createMany({
+    data: [
+      {
+        name: 'Schlaefli Roland',
+        email: 'roland.schlaefli@df.uzh.ch',
+      },
+      {
+        name: 'Weber Maximilian',
+        email: 'maximilian.weber@df.uzh.ch',
+      }
+    ]
+  })
+
+  console.log({applicationStatus, proposalStatus, proposalType, topicArea, proposalFeedbackType, responsible})
+
+  console.log('> finished prisma seed')
 }
 
 seed(prismaClient)
