e5d4cbeffa1d46d2351d0ca6fd46d79f2dd0b044
mxmlnwbr <maximilian.weber@bluewin.ch>
2025-10-08 14:49:00 +0200

    feat: add additional faculty members to seed data for IBW department

--- prisma/seed-ibw.ts
+++ prisma/seed-ibw.ts
@@ -86,6 +86,161 @@ async function seed(prisma: PrismaClient) {
         email: 'ibw-srv-powplatf-prd@d.uzh.ch',
         department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
       },
+      {
+        name: 'Giuffredi-Kähr Andrea',
+        email: 'andrea.giuffredi-kaehr@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Scherer Andreas Georg',
+        email: 'andreas.scherer@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Schulze Anja',
+        email: 'anja.schulze@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Brenøe Anne A.',
+        email: 'anne.brenoe@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Segal Carmit',
+        email: 'carmit.segal@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Barz Christiane',
+        email: 'christiane.barz@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Oesch David',
+        email: 'david.oesch@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Seidl David',
+        email: 'david.seidl@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Pfaff Dieter',
+        email: 'dieter.pfaff@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Franck Egon',
+        email: 'egon.franck@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Petrov Evgeny',
+        email: 'evgeny.petrov@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Dietl Helmut M.',
+        email: 'helmut.dietl@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Chen Hui',
+        email: 'hui.chen@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Menges Jochen',
+        email: 'jochen.menges@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Luger Johannes',
+        email: 'johannes.luger@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Zigova Katarina',
+        email: 'katarina.zigova@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Howe Lauren C.',
+        email: 'lauren.howe@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Aguiar Luis',
+        email: 'luis.aguiar@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Natter Martin',
+        email: 'martin.natter@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Prokopyev Oleg',
+        email: 'oleg.prokopyev@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Lehnert Patrick',
+        email: 'patrick.lehnert@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Flepp Raphael',
+        email: 'raphael.flepp@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Algesheimer René',
+        email: 'rene.algesheimer@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Eberle Reto',
+        email: 'reto.eberle@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Göx Robert',
+        email: 'robert.goex@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Tan Tarkan',
+        email: 'tarkan.tan@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Keil Thomas',
+        email: 'thomas.keil@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Backes-Gellner Uschi',
+        email: 'ubg@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Kaiser Ulrich',
+        email: 'ulrich.kaiser@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Rauter Thomas',
+        email: 'thomas.rauter@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      },
+      {
+        name: 'Mohring Uta',
+        email: 'uta.mohring@business.uzh.ch',
+        department: process.env.NEXT_PUBLIC_DEPARTMENT_NAME as Department,
+      }
     ],
   })
   console.log('Responsible table seeded successfully')
