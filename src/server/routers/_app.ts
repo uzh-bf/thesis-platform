1735972eaa68fe00a578fd31e1da6534329abe68
mxmlnwbr <maximilian.weber@bluewin.ch>
2025-11-14 10:06:41 +0100

    fix: use ISO format for proposal submission date

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -419,15 +419,7 @@ export const appRouter = router({
       })
     )
     .mutation(async ({ ctx, input }) => {
-      const submitDate = new Date().toLocaleString('en-US', {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        hour12: true
-      })
+      const submitDate = new Date().toISOString()
 
       const payload = {
         responder: input.responder,
